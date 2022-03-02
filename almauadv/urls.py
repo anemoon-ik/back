ad531e996e9a5da4c062f6d1f5932120966fa7dd
anemoon-ik <sunny.adinay@mail.ru>
2022-03-02 14:27:31 +0600

    working crud for trips

--- almauadv/urls.py
+++ almauadv/urls.py
@@ -1,19 +1,22 @@
 from django.contrib import admin 
 from django.http import HttpRequest, HttpResponse  
 from django.urls import path  
-from app.views import (
+from app.views_cars import (
     index,
     delete,
     add_car,
+)
+
+from app.views_trips import (
     car_trips,
-    add_trip
+    car_trips_delete
 )
 
 urlpatterns = [
     path('', index),
     path('delete/<str:id_>', delete),
     path('add_car/', add_car),
-    path('cars/<int:id_>', car_trips),
-    path('add_trip/', add_trip),
-    path('admin777/', admin.site.urls),
+    path('cars/<int:car_id>/trips', car_trips),
+    path('delete_trip/<int:trip_id>', car_trips_delete),
+    path('admin/', admin.site.urls),
 ]
--- app/views_cars.py
+++ app/views_cars.py
@@ -12,12 +12,6 @@ def index(request: HttpRequest) -> HttpResponse:
         "cars": catalog
     })
 
-def trips(request: HttpRequest) -> HttpResponse:
-    catalog1 = Trip.objects.all()
-    return render(request, "trips.html", context={
-        "trips": catalog1
-    })
-
 
 def delete(request: HttpRequest, id_: str) -> HttpResponse:
     global cars
@@ -42,21 +36,3 @@ def add_car(request: HttpRequest) -> HttpResponse:
     return redirect("/")
 
 
-def add_trip(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        if request.POST.get("id_", "") == "":
-            Trip.objects.create(
-                date=request.POST.get("date", ""),
-                km=request.POST.get("km", ""),
-            )
-        else:
-            trip = Trip.objects.get(pk=int(request.POST.get("id_", "")))
-            trip.date = request.POST.get("date", "")
-            trip.km = request.POST.get("km", "")
-            trip.save()
-    return redirect("/")
-
-
-
-def car_trips(request: HttpRequest, id_: int) -> HttpResponse:
-    return render(request, "trips.html", {"trips": trips})
